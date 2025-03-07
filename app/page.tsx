1ca6545080d4e9244f32c2da226ac9f86c4482e8
HaiderGhauri <haideralikhanh70@gmail.com>
2025-03-07 14:11:14 +0500

    set folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import { Card } from "./components/Card";
-import { Features } from "./components/Features";
-import { Gallery } from "./components/Gallery";
-import { HeroSection } from "./components/Hero";
-import { Testimonials } from "./components/Testimonials";
+import { Card } from "./components/home/Card";
+import { Features } from "./components/home/Features";
+import { Gallery } from "./components/home/Gallery";
+import { HeroSection } from "./components/home/Hero";
+import { Testimonials } from "./components/home/Testimonials";
 
 export default function Home() {
   return (
